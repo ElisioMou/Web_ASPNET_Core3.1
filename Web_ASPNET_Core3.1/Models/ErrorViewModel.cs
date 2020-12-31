48a040916a154f3d2fd8cc61560e3ad48ce345f3
Elisio Moura <elisiomou@gmail.com>
2020-12-30 22:28:38 -0300

    Inicio do projeto Web_ASPNET_Core3.1

---
+++ Web_ASPNET_Core3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_ASPNET_Core3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
