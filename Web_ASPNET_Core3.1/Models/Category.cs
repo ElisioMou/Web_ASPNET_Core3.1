5fa73f4590716324a65a924c7f2b7ef64366064e
Elisio Moura <elisiomou@gmail.com>
2021-01-02 22:05:34 -0300

    Finalizado o projeto ASPNET.MVC e a integração com o banco de dados.

--- Web_ASPNET_Core3.1/Models/Category.cs
+++ Web_ASPNET_Core3.1/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Web_ASPNET_Core3._1.Models
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
         public List<Product> Products { get; set; }
     }
--- Web_ASPNET_Core3.1/Models/Product.cs
+++ Web_ASPNET_Core3.1/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Web_ASPNET_Core3._1.Models
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
+        [Range(1,10, ErrorMessage = "Valor fora da faixa")]
         public string Description { get; set; }
         public int Quantity { get; set; }
         public int CategoryId { get; set; }
